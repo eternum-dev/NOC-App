100dc0fa48c3eb4c9a7380ee9d031f9a45faa454
eternum-dev <alejandro.thon.j@gmail.com>
2024-06-11 21:55:02 -0400

    create file

---
+++ src/domains/use-cases/email/send-email.ts
@@ -0,0 +1,49 @@
+import { error } from 'console';
+import { EmailService } from '../../../presentation/email/email.service';
+import { LogRepository } from '../../repository/log.repository';
+import { LogEntity, LogSeveritylevel } from '../../entities/log.entity';
+interface SendLogEmailUsecase {
+    execute: (to: string | string[]) => Promise<boolean>
+}
+
+
+export class SendEmailLogs implements SendLogEmailUsecase {
+
+    constructor(
+        private readonly emailService: EmailService,
+        private readonly logRepository: LogRepository,
+    ) { }
+
+    async execute(to: string | string[]) {
+
+        try {
+            const sent = await this.emailService.sendEmailWithFileSystemLog(to);
+            if (!sent) {
+                throw new Error('email not set');
+            }
+
+            const log = new LogEntity({
+                level: LogSeveritylevel.low,
+                message: `${error}`,
+                origin: 'send-email.ts'
+            });
+
+            this.logRepository.saveLogs(log);
+
+            return true;
+
+        } catch (error) {
+            const log = new LogEntity({
+                level: LogSeveritylevel.high,
+                message: `${error}`,
+                origin: 'send-email.ts'
+            });
+
+            this.logRepository.saveLogs(log);
+
+            return false;
+        }
+
+
+    }
+}
