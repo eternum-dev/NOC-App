95e5c6d1fa1f774c45e1c190ee4cc759fb85cb3c
eternum-dev <alejandro.thon.j@gmail.com>
2024-06-25 20:15:56 -0400

    TEST(env): add test for env options.
    - Added a test to ensure the environment variables are correctly set.
    - The test checks that the envs object contains the expected values for various configuration options."

---
+++ src/config/plugins/envs.plugin.test.ts
@@ -0,0 +1,19 @@
+import { envs } from "./envs.plugin";
+
+
+
+describe('Pruebas en envs.plugin', () => {
+
+    test('should return env options', () => {
+        expect(envs).toEqual({
+            PORT: 3000,
+            MAILER_SERVICE: 'gmail',
+            MAILER_EMAIL: 'alejandro96121@gmail.com',
+            MAILER_SECRET_KEY: 'ceglcqzrnumjytln',
+            MONGO_URL: 'mongodb://alejandro:123456@localhost:27018',
+            MONGO_USER: 'alejandro',
+            MONGO_PASS: '123456',
+            MONGO_DB_NAME: 'NOC-test'
+        });
+    });
+}); 
