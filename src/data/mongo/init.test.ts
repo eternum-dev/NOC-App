7e5d5de898f1a67fc610de2f31fc8aff0490d1dc
eternum-dev <alejandro.thon.j@gmail.com>
2024-06-25 20:12:44 -0400

    TEST(db): Add test for connection initiation in mongo.

---
+++ src/data/mongo/init.test.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import { MongoDataBase } from "./init";
+
+
+
+describe('Pruebas en init.test.ts', () => {
+
+    afterAll(()=> {
+        mongoose.connection.close();
+    });
+
+
+    test('should connect to MongoDB', async () => {
+        const connected = await MongoDataBase.connect({
+            dbName: process.env.MONGO_DB_NAME!,
+            mongoUrl: process.env.MONGO_URL!
+        });
+
+        expect(connected).toBeTruthy();
+        console.log(connected);
+    });
+    test('should throw an error ', async () => {
+
+        try {
+            const connected = await MongoDataBase.connect({
+                dbName: process.env.MONGO_DB_NAME!,
+                mongoUrl: 'mongodb://alejandro:1234568712@localhost:27018'
+            });
+            expect(true).toBeFalsy();
+        } catch (error) {
+
+        }
+
+    });
+});  
